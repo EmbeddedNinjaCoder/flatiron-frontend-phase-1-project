3121de141620c57635ab8b9b8e57ca4a2b8e4ba7
German Santos <santos.german@gmail.com>
2023-06-27 16:59:43 -0700

    Add focus event listener to form and format

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("Connected")
+console.log("Connected");
+
+const handleFormInputFocus = () => {
+  console.log("focus ocurred");
+};
+
+const searchTermsInput = document.body.querySelector("#search-terms");
+searchTermsInput.addEventListener("focus", handleFormInputFocus);
