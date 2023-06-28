96fb455f8bb956929cb751f23d49e46f9374899b
German Santos <santos.german@gmail.com>
2023-06-27 19:28:49 -0700

    Add fetch template

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 console.log("Connected");
 
-const handleFormInputFocus = () => {
+const searchTermsInput = document.body.querySelector("#search-terms");
+
+const getMealCategories = async () => {
+    const mealCategoriesApiURL = "https://www.themealdb.com/api/json/v1/1/categories.php"
+    try {
+        const response = await fetch(mealCategoriesApiURL)
+        // console.log(response) 
+        const data = response.json()
+        console.log(`data: `, data)
+    } catch(error) {
+        console.log(error)
+        alert('Something went wrong, try again later')
+    }   
+}   
+
+const handleFormInputFocus = async () => {
   console.log("focus ocurred");
+
+  await getMealCategories();
 };
 
-const searchTermsInput = document.body.querySelector("#search-terms");
+
 searchTermsInput.addEventListener("focus", handleFormInputFocus);
