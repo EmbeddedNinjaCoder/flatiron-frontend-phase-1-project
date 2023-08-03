7c9f6b4cbbe959d58906f43e4451baeb93aaafae
German Santos <santos.german@gmail.com>
2023-08-03 14:04:21 -0700

    Add generic fetch to public api and console render

--- main.js
+++ main.js
@@ -2,38 +2,39 @@ console.log("Connected");
 
 const searchTermsInput = document.body.querySelector("#search-terms");
 
-
-
 // Fetch meal categories data from TheMealDB API
 const getMealCategories = async () => {
-    const mealCategoriesApiURL = "https://www.themealdb.com/api/json/v1/1/categories.php"
-    try {
-        const response = await fetch(mealCategoriesApiURL)
-        //const data = await fetch(mealCategoriesApiURL).then(response => response.json())
-        // console.log(response) 
-        const data = await response.json()
-        const categories = await data.categories
-        console.log(`categories: `, categories)
-        return categories
-    } catch(error) {
-        console.log(error)
-        alert('Something went wrong, try again later')
-    }   
-    
-}  
+  const mealCategoriesApiURL =
+    "https://www.themealdb.com/api/json/v1/1/categories.php";
+  try {
+    const response = await fetch(mealCategoriesApiURL);
+    //console.log(response);
+    const data = await response.json();
+    //console.log("data: ", data)
+    const categories = data.categories;
+    console.log(`categories: `, categories);
+    return categories;
+  } catch (error) {
+    console.log(error);
+    alert("Something went wrong, try again later");
+  }
+};
 
 // Render meal categories data to DOM
-const renderMealCAtegories = (mealCategoriesObj) => {
-    console.log('renderMealCategories')
-    console.table(mealCategoriesObj)
-}
+const renderMealCategories = (mealCategoriesObj) => {
+  console.log("renderMealCategories");
+  console.table(mealCategoriesObj);
+
+  //   Create a wrapping element - class of card
+};
 
 // Function to call when form input is given focus
 const handleFormInputFocus = async () => {
   console.log("focus ocurred");
 
+  //   const mealCategoriesObj =
   const mealCategoriesObj = await getMealCategories();
-  renderMealCAtegories(mealCategoriesObj);
+  renderMealCategories(mealCategoriesObj);
 };
 
 // Add event listener to search term input
---
+++ scratch.md
@@ -0,0 +1,23 @@
+## Phase 1 Project Notes
+
+Requirements
+
+Phase 1 Project
+https://my.learn.co/courses/649/assignments/32803?module_item_id=82993
+
+Project Guidelines
+https://my.learn.co/courses/649/pages/phase-1-project-guidelines?module_item_id=82990
+
+-html css js
+-public api or db.json
+-2 distinct event listeners (bonus - 3+)
+-iterating through arrays
+-need to use array methods
+-single page only
+-30+ commits
+-render data in the HTML DOM
+-5 objects
+
+Extras
+
+-mobile friendly (bonus)
