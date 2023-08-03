1e49ee3295c566fbe0df4d963902abb756182da0
German Santos <santos.german@gmail.com>
2023-08-03 15:24:33 -0700

    Add HTML template code for api render to DOM

--- main.js
+++ main.js
@@ -21,11 +21,30 @@ const getMealCategories = async () => {
 };
 
 // Render meal categories data to DOM
-const renderMealCategories = (mealCategoriesObj) => {
+const renderMealCategories = (mealCategoriesArray) => {
   console.log("renderMealCategories");
-  console.table(mealCategoriesObj);
+  console.table(mealCategoriesArray);
 
-  //   Create a wrapping element - class of card
+  // find and select a UL with meal categories ID DOM element to append my data into
+
+  // for each element in our meal categories array
+  // create a wrapping element maybe <article> class of card
+  // appendChild  the article.card to DOM elemtn
+  // display the category name, image and description
+  // image
+  // create an img element
+  // set img src to category thumbnail url
+  // give class of category-thumbnail
+  // give it a mobile friendly max width of 300px
+  // appendChild image to card
+  // name
+  // create an h4 element
+  // set h4 element textContent to be meal category name
+  // appendChild h4 to card
+  // description
+  // create p element
+  // set p element textContent to be meal category description
+  // appendChild p to card
 };
 
 // Function to call when form input is given focus
@@ -33,8 +52,8 @@ const handleFormInputFocus = async () => {
   console.log("focus ocurred");
 
   //   const mealCategoriesObj =
-  const mealCategoriesObj = await getMealCategories();
-  renderMealCategories(mealCategoriesObj);
+  const mealCategoriesArray = await getMealCategories();
+  renderMealCategories(mealCategoriesArray);
 };
 
 // Add event listener to search term input
