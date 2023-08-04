7575c15d13caab495db32dbb31a7d6a6f347ac90
German Santos <santos.german@gmail.com>
2023-08-04 06:22:08 -0700

    Render fetch data to first dropdown

--- main.js
+++ main.js
@@ -12,8 +12,20 @@ getCuisines();
 function getCuisines() {
   fetch("https://www.themealdb.com/api/json/v1/1/list.php?a=list")
     .then((r) => r.json())
-    .then((cuisines) => console.log(cuisines))
+    .then((cuisines) => renderCuisineOptions(cuisines.meals))
     .catch();
+}
+
+function renderCuisineOptions(cuisines) {
+  //console.log(cuisines);
+  cuisines.forEach((cuisine) => {
+    const option = document.createElement("option");
+    option.value = cuisine.strArea;
+    option.textContent = cuisine.strArea;
+    console.log(option);
+    cuisineSelect.append(option);
+  });
+  // console.log(cuisines);
 }
 
 // ---------------------------------------------------------------------
